dd3a6888684a6fd3f1931eccdd91d17eb6593a70
rohan472000 <anand00rohan@gmail.com>
2023-01-01 20:32:24 +0530

    committed to main

---
+++ dags/dag.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+# from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+from training import training
+from evaluation import evaluate
+
+default_args = {
+    'owner': 'NASDAQ-MODEL',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+with DAG(
+        dag_id='ml_project',
+        default_args=default_args,
+        description='ML project',
+        schedule_interval='* * * * *',
+        start_date=datetime(2020, 1, 6)
+) as dag:
+    # enter_point = DummyOperator(
+    #     task_id='enter_point'
+    # )
+    train_model = PythonOperator(
+        task_id='train_model',
+        python_callable=training
+    )
+
+    evaluate_model = PythonOperator(
+        task_id='evaluate_model',
+        python_callable=evaluate
+    )
+
+    """enter_point >>"""
+    train_model >> evaluate_model
---
+++ dags/evaluation.py
@@ -0,0 +1,34 @@
+import logging
+from pathlib import Path, PosixPath
+import pandas as pd
+from datetime import date
+from joblib import load
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+from training import MODEL_PATH
+from training import get_data, give_ytrain_ytest
+from sklearn.preprocessing import MinMaxScaler
+
+
+def evaluate():
+    df = get_data()
+    m = give_ytrain_ytest()
+    model_path = Path(MODEL_PATH)
+    model = load_model(model_path.joinpath(f'model_{date.today().isoformat()}.joblib'))
+
+    evaluate_model(model, df, m)
+
+
+def load_model(model_path: PosixPath) -> Pipeline:
+    return load(model_path)
+
+
+def evaluate_model(model: Pipeline, df: pd.DataFrame, m: list):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    predictions = model.predict(df)
+    train_predict = scaler.inverse_transform(predictions)
+    
+    logging.info(f'>>>> Mean squared error: {mean_squared_error(m, train_predict)}')
+
