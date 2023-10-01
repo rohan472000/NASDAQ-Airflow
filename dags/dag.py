588aebd5b1e060d9d086ed7c1238def1f73deb95
Shrey Raj <119098647+Shrey-Raj@users.noreply.github.com>
2023-10-01 13:03:49 +0530

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -9,26 +9,19 @@ from training import training
 from evaluation import evaluate
 
 default_args = {
-    'owner': 'NASDAQ-MODEL',
-    'retries': 6,
-    'retry_delay': timedelta(minutes=4)
+    "owner": "NASDAQ-MODEL",
+    "retries": 6,
+    "retry_delay": timedelta(minutes=4),
 }
 with DAG(
-        dag_id='ml_project',
-        default_args=default_args,
-        description='ML project',
-        schedule_interval='* * * * *',
-        start_date=datetime(2020, 1, 6)
+    dag_id="ml_project",
+    default_args=default_args,
+    description="ML project",
+    schedule_interval="* * * * *",
+    start_date=datetime(2020, 1, 6),
 ) as dag:
-    train_model = PythonOperator(
-        task_id='train_model',
-        python_callable=training
-    )
+    train_model = PythonOperator(task_id="train_model", python_callable=training)
 
-    evaluate_model = PythonOperator(
-        task_id='evaluate_model',
-        python_callable=evaluate
-    ) 
+    evaluate_model = PythonOperator(task_id="evaluate_model", python_callable=evaluate)
 
- 
     train_model >> evaluate_model
