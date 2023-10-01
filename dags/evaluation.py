9289ab2fa1c24fb6f678cc52c9ccbe4eb0582afc
Shrey Raj <119098647+Shrey-Raj@users.noreply.github.com>
2023-10-01 15:45:08 +0530

    Update evaluation.py

--- dags/evaluation.py
+++ dags/evaluation.py
@@ -3,27 +3,20 @@ from pathlib import Path, PosixPath
 import pandas as pd
 from datetime import date
 from joblib import load
-
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-
-from training import MODEL_PATH
-from training import get_data, give_ytrain_ytest
+from training import MODEL_PATH, get_data, give_ytrain_ytest
 from sklearn.preprocessing import MinMaxScaler
-
 
 def evaluate():
     df = get_data()
     m = give_ytrain_ytest()
     model_path = Path(MODEL_PATH)
     model = load_model(model_path.joinpath(f'model_{date.today().isoformat()}.joblib'))
-
     evaluate_model(model, df, m)
-
 
 def load_model(model_path: PosixPath) -> Pipeline:
     return load(model_path)
-
 
 def evaluate_model(model: Pipeline, df: pd.DataFrame, m: list):
     scaler = MinMaxScaler(feature_range=(0, 1))
@@ -31,4 +24,3 @@ def evaluate_model(model: Pipeline, df: pd.DataFrame, m: list):
     train_predict = scaler.inverse_transform(predictions)
     
     logging.info(f'>>>> Mean squared error: {mean_squared_error(m, train_predict)}')
-
